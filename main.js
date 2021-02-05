f5bedff4f3d209c7f60e612662fcb8ce9d7056a8
Kraivit Mongkhonsakunrit <kraivit.m@edvisory.co.th>
2021-02-05 22:59:48 +0700

    all

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const {Block} = require('./lib/Block')
 
 const blockchain = new Blockchain();
 console.log("Slow and Steady....")
-blockchain.addBlock(new Block(1,{candidate:"นายก",resources:{amout:10}}));
+blockchain.createBlock(new Block(1,{candidate:"นายก",resources:{amout:10}}));
 console.log("Slow and Steady....")
-blockchain.addBlock(new Block(2,{candidate:"นายข",resources:{amout:20}}));
+blockchain.createBlock(new Block(2,{candidate:"นายข",resources:{amout:20}}));
 
